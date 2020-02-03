75a0ac75da23078016fa4c33c246d919cf934827
IbrahimG88 <goharyibrahim@gmail.com>
2020-02-03 12:48:05 +0200

    used flask db migrate and upgrade

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
 # for debugging within the Todo class:
     def __repr__(self):
